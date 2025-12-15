620a9167ba54178b70b04f2af24b31847a6cd328
v2vv <v2vvcn@gmail.com>
2025-12-15 13:10:11 +0800

    将Tabbar设为单独组件

---
+++ src/components/tabbar/MyTabbar.tsx
@@ -0,0 +1,19 @@
+import { Tabbar } from '@nutui/nutui-react-taro'
+import { Cart, HeartFill, Heart, Hi, Home, User } from '@nutui/icons-react-taro'
+
+const MyTabbar = () => {
+  return (
+    <Tabbar fixed>
+      <Tabbar.Item title="首页" icon={<Home />} />
+      <Tabbar.Item title="逛" icon={<Hi />} />
+      <Tabbar.Item
+        title="收藏"
+        icon={(active) => (active ? <HeartFill /> : <Heart />)}
+      />
+      <Tabbar.Item title="购物车" icon={<Cart />} />
+      <Tabbar.Item title="我的" icon={<User />} />
+    </Tabbar>
+  )
+}
+
+export default MyTabbar
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,19 +1,8 @@
-import React from 'react'
-import { Tabbar } from '@nutui/nutui-react-taro'
-import { Cart, HeartFill, Heart, Hi, Home, User } from '@nutui/icons-react-taro'
+import MyTabbar from '../../components/tabbar/MyTabbar'
 
 const Index = () => {
   return (
-    <Tabbar fixed>
-      <Tabbar.Item title="首页" icon={<Home />} />
-      <Tabbar.Item title="逛" icon={<Hi />} />
-      <Tabbar.Item
-        title="收藏"
-        icon={(active) => (active ? <HeartFill /> : <Heart />)}
-      />
-      <Tabbar.Item title="购物车" icon={<Cart />} />
-      <Tabbar.Item title="我的" icon={<User />} />
-    </Tabbar>
+    <MyTabbar />
   )
 }
 
